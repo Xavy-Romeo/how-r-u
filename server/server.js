b4d819d9fd878a90d8cfab8d343117861a4f238e
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-13 22:08:04 -0700

    created server and mongoose connections

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3111;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`🌍 Now listening on localhost:${PORT}`);
+        // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
