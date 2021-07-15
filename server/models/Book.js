c47dbb15e4ebe51a732a1586d8f2385875fa480f
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-14 18:06:40 -0700

    added Book model

--- server/models/Book.js
+++ server/models/Book.js
@@ -3,7 +3,22 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    emotion: {
+        type: Schema.Types.ObjectId,
+        ref: 'Emotion',
+        required: true
+    }
 });
 
 const Book = mongoose.model('Book', bookSchema);
--- server/models/Emotion.js
+++ server/models/Emotion.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const emotionSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    }
 });
 
 const Emotion = mongoose.model('Emotion', emotionSchema);
