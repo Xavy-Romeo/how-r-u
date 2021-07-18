500e6ca6ef56f54e2454219af87c44d2910c366f
Wayne Edwards <waedwards42@yahoo.com>
2021-07-17 23:45:18 -0700

    added more compenents. Edited app.js. Trouble loading server

---
+++ client/src/components/Anxious/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Anxious() {
+    return (
+        <div>
+            <h1>Anxious</h1>
+            
+        </div>
+    )
+}
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Sad() {
+export default function Home() {
     return (
         <div>
-            <h1>Sad</h1>
+            <h1>Home</h1>
             
         </div>
     )
