d8491a8c8f5a6e26c43db8497eb52657bedc3902
Wayne Edwards <waedwards42@yahoo.com>
2021-07-18 19:32:44 -0700

    added code to Happy index.js to duplicate

--- client/src/components/Happy/index.js
+++ client/src/components/Happy/index.js
@@ -1,10 +1,98 @@
 import React from 'react';
+import './src/App.css';
+
+
+import Button from "@material-ui/core/Button";
+
+
+import { makeStyles, ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { green, orange } from "@material-ui/core/colors";
+
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+
+
+const theme = createMuiTheme({
+    typography: {
+      h2: {
+        fontSize: 24,
+      }
+    },
+    palette: {
+      primary: {
+        main: green[400]
+      },
+      secondary: {
+        main: orange[400]
+      }
+    }
+  })
+
+
 
 export default function Happy() {
     return (
-        <div>
-            <h1>Happy</h1>
-            
-        </div>
+        <div> return (
+            <ThemeProvider theme={theme}>
+              <Container maxWidth='lg'>
+            <div className="App">
+              <header className="App-header">
+                <AppBar color="secondary">
+                  <Toolbar>
+                   
+                    <Typography variant="h6">
+                      How Are You Today?
+                    </Typography>
+                    <Button>
+                    Home
+                    </Button>
+                  </Toolbar>
+                </AppBar>
+                <Typography variant="h2" component="div">
+                  Happy
+                </Typography>    
+                <Typography variant="subtitle1">
+                Tools and Techniques to Keep you Moving!
+                </Typography>
+              
+              
+                <Grid container spacing={8} justify='center'>
+                <Grid item xs={12} sm={6}>
+                  <Paper style={{ height:75, width:'100%' }} />
+                  <h2>Meditation</h2>
+                </Grid>
+                <Grid item xl={12}>
+                  <Paper style={{ height:75, width:50 }} />
+                  <h2>Activities</h2>
+                </Grid>
+                <Grid item >
+                  <Paper style={{ height:75, width:50 }} />
+                </Grid>
+                <Grid item>
+                  <Paper style={{ height:75, width:50 }} />
+                  <h2>Books</h2>
+                </Grid>
+              
+                </Grid>
+              
+              
+               
+               
+              
+              </header>
+            </div>
+            </Container>
+            </ThemeProvider>
+          )
+          </div>
     )
-}
+        }
+          
+ 
+       
+    
