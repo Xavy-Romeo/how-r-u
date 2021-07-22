f64163c1baf016ad637ce84cfbb6c28365c39b87
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-22 11:16:21 -0700

    login modal

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
 import Button from "@material-ui/core/Button";
+import './Login.css';
 
 export default function Login({ classes }) {
      
     const [modalOpen, setModalOpen] = useState(false);
+
+    if (modalOpen) {
+        document.body.classList.add('modal-active');
+    }
+    else {
+        document.body.classList.remove('modal-active');
+    }
     
     return (
         <>
-            <Button
-                onClick={() => {setModalOpen(true)}}
-                variant="contained"
-                color="primary"
-                className={classes.button}
-            >
-                Login
-            </Button>
+            {!modalOpen && (
+                <Button
+                    onClick={() => {setModalOpen(true)}}
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                >
+                    Login
+                </Button>
+            )}
 
             {modalOpen && (
-                <div>
+                <div className="login-modal">
                     <div className="overlay"></div>
-                    <div>
-                        <h2> HELLO HELLO HELLO MODAL!!!!</h2>
+                    <div className="login-container">
+                        <form className="login-form"> 
+                            <h3>Login</h3>
+                            
+                            <label for="username">Username:</label>
+                            <input type="text" placeholder="Username" name="username"></input>
+
+                            <label for="password">Password:</label>
+                            <input type="text" placeholder="Password" name="password"></input>
+                            <div className="button-div">
+                                <button type="submit" onClick={() => {setModalOpen(false)}}>Login</button>   
+                            </div>
+                        </form>
                     </div>
-                    <button onClick={() => {setModalOpen(false)}}></button>
+                    
                 </div>
                 
             )}
